c251e9ddcd31df6f16102f371409bb57008ebf43
Runlin Xiao <rxiao13@asu.edu>
2021-10-02 17:46:27 -0700

    updating

--- front-end/soccer/src/index.js
+++ front-end/soccer/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react';
 import App from './App';
 import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store'
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
