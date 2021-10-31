a8b325a9b60123bc6f4c470741d9266cfed553ac
rahulvuppla <79802988+rahulvuppla@users.noreply.github.com>
2021-10-31 01:38:08 -0700

    https://meet.google.com/pce-awsw-sfx repositories

--- back-end/src/main/java/com/ser515/soccer/database/datamodel/User.java
+++ back-end/src/main/java/com/ser515/soccer/database/datamodel/User.java
@@ -17,7 +17,7 @@ public class User {
 
     private String firstName;
     private String lastName;
-    
+
     private String password;
     private String emailAddress;
     private String phoneNo;
