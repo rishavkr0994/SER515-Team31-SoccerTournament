6bfb9cf169451e9a5264e0878b513fbdc3c0d17a
rishavkr0994 <89811541+rishavkr0994@users.noreply.github.com>
2021-10-30 21:55:06 -0700

    Fix front-end errors due to improper merge
    Signed-off-by: rishavkr0994 <89811541+rishavkr0994@users.noreply.github.com>

---
+++ front-end/soccer/src/components/TLeft.js
@@ -0,0 +1,17 @@
+import { Paper } from '@mui/material';
+import React from 'react';
+export default function TLeft (params)  {
+    return(
+        <Paper elevation = {3}>
+            <div>
+                {/* <p>afasd</p>
+                <p>afasd</p>
+                <p>afasd</p>
+                <p>afasd</p>
+                <p>afasd</p>
+                <p>afasd</p> */}
+
+            </div>
+        </Paper>
+    )
+}
