2e661749d6eff961553681705088558beb8fd80a
rishavkr0994 <89811541+rishavkr0994@users.noreply.github.com>
2021-10-31 03:12:33 -0700

    Added Swagger 2 For API Documentation
    Signed-off-by: rishavkr0994 <89811541+rishavkr0994@users.noreply.github.com>

--- back-end/src/main/java/com/ser515/soccer/rest/TeamAPI.java
+++ back-end/src/main/java/com/ser515/soccer/rest/TeamAPI.java
@@ -3,6 +3,8 @@ package com.ser515.soccer.rest;
 import com.ser515.soccer.database.repository.TeamRepository;
 import com.ser515.soccer.rest.datamodel.APIResponseBody;
 import com.ser515.soccer.rest.datamodel.TeamRegistrationRequestBody;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController @RequestMapping("/rest/team")
+@Api(value="Team API", description="Rest APIs For Team Management")
 public class TeamAPI {
     @Autowired TeamRepository teamRepository;
 
+    @ApiOperation(value = "Register a team with team information")
     @PostMapping("/registration")
     public ResponseEntity<Object> registration(@RequestBody TeamRegistrationRequestBody requestBody) {
         if (teamRepository.existsByName(requestBody.name))
