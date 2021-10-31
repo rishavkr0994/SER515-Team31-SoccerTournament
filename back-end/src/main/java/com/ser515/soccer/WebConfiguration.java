86d026644324b1d8862f5fcbdbd7603b5a52e40b
rishavkr0994 <89811541+rishavkr0994@users.noreply.github.com>
2021-10-30 20:19:45 -0700

    Revert "Revert "Merge branch 'main' of https://github.com/rishavkr0994/SER515-Team31-SoccerTournament into Tournament""
    This reverts commit e45e4be4c2878e0046b8ef0240900f93001f3bb7.

---
+++ back-end/src/main/java/com/ser515/soccer/WebConfiguration.java
@@ -0,0 +1,14 @@
+package com.ser515.soccer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration @EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
--- back-end/src/main/java/com/ser515/soccer/database/datamodel/Match.java
+++ back-end/src/main/java/com/ser515/soccer/database/datamodel/Match.java
@@ -7,7 +7,8 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.util.Date;
 
-@Entity @Getter @Setter @NoArgsConstructor @Table(name = "AMatch")
+@Entity @Table
+@Getter @Setter @NoArgsConstructor
 public class Match {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
