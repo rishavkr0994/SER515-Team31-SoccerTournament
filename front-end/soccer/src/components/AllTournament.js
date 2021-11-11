8b335613ba0b55c04edff61c47ab8af9422fc734
Runlin Xiao <rxiao13@asu.edu>
2021-11-11 16:48:26 -0700

    US112-Task113 showing all tournaments page

--- front-end/soccer/src/components/AllTournament.js
+++ front-end/soccer/src/components/AllTournament.js
@@ -1,11 +1,108 @@
-
+import { Grid } from '@material-ui/core';
 import { Button } from '@mui/material';
 import React from 'react';
 import './FooterAndMain.css'
+import TournamentCard from './TournamentCard';
+
+const rows = [
+    {
+        src:"tcard2.jpeg",
+        name:"Century Hitters Tournament",
+        description:"This is a century tournament!!!",
+        startDate:["11","22","44"],
+        endDtate:["00","11","22"],
+        timeRange:"January 14th - 16th, 2022",
+        endRegistration:"December 21, 2021",
+      },
+      {
+        src:"tcard1.png",
+        name:"Supersixers League",
+        description:"Come and join this excited tournament!",
+        startDate:["11","22","44"],
+        endDtate:["00","11","22"],
+        timeRange:"January 14th - 16th, 2022",
+        endRegistration:"December 21, 2021",
+        isOnGoing:"true"
+      },
+      {
+        src:"tcard3.jpeg",
+        name:"Delight Wings Gaming",
+        description:"Come and join this excited tournament!",
+        startDate:["11","22","44"],
+        endDtate:["00","11","22"],
+        timeRange:"January 14th - 16th, 2022",
+        endRegistration:"December 21, 2021"
+      },{
+        src:"tcard2.jpeg",
+        name:"Century Hitters Tournament",
+        description:"This is a century tournament!!!",
+        startDate:["11","22","44"],
+        endDtate:["00","11","22"],
+        timeRange:"January 14th - 16th, 2022",
+        endRegistration:"December 21, 2021",
+      },
+      {
+        src:"tcard1.png",
+        name:"Supersixers League",
+        description:"Come and join this excited tournament!",
+        startDate:["11","22","44"],
+        endDtate:["00","11","22"],
+        timeRange:"January 14th - 16th, 2022",
+        endRegistration:"December 21, 2021",
+        isOnGoing:"true"
+      },
+      {
+        src:"tcard3.jpeg",
+        name:"Delight Wings Gaming",
+        description:"Come and join this excited tournament!",
+        startDate:["11","22","44"],
+        endDtate:["00","11","22"],
+        timeRange:"January 14th - 16th, 2022",
+        endRegistration:"December 21, 2021"
+      },{
+        src:"tcard2.jpeg",
+        name:"Century Hitters Tournament",
+        description:"This is a century tournament!!!",
+        startDate:["11","22","44"],
+        endDtate:["00","11","22"],
+        timeRange:"January 14th - 16th, 2022",
+        endRegistration:"December 21, 2021",
+      },
+      {
+        src:"tcard1.png",
+        name:"Supersixers League",
+        description:"Come and join this excited tournament!",
+        startDate:["11","22","44"],
+        endDtate:["00","11","22"],
+        timeRange:"January 14th - 16th, 2022",
+        endRegistration:"December 21, 2021",
+        isOnGoing:"true"
+      },
+      {
+        src:"tcard3.jpeg",
+        name:"Delight Wings Gaming",
+        description:"Come and join this excited tournament!",
+        startDate:["11","22","44"],
+        endDtate:["00","11","22"],
+        timeRange:"January 14th - 16th, 2022",
+        endRegistration:"December 21, 2021"
+      }
+  ];
+
 
 export default function AllTournament(){
     return(
-        <div className="Main">
+        <div className="Main" style={{marginTop:"30px"}}>
+            <div style={{height:"30px"}}>
+                </div>
+            <Grid container spacing={3}>
+            {rows.map((row) => (
+                <Grid item sx={3}>
+              <TournamentCard key={row.name} tournament={row} />
+                </Grid>
+            ))}
+            </Grid>
+
             <Button variant="contained" href="/tournament/create">
               Add Tournament
             </Button>
