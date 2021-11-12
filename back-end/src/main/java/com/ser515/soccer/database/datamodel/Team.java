cea5c760da9c28a16236415fcdc338eb116fc726
rishavkr0994 <89811541+rishavkr0994@users.noreply.github.com>
2021-11-12 04:21:14 -0700

    [Back-End] Linked tournament to team entity + Update team information to tournament on team registration
    - Added tournament name to team registration request to identify the tournament for which the team is registering
    - Removed team name unique constraint to allow same team name across different tournaments
    - Persisted enum as string to database for better readability
    - Removed unused function existsByName form TeamRepository
    
    Signed-off-by: rishavkr0994 <89811541+rishavkr0994@users.noreply.github.com>

--- back-end/src/main/java/com/ser515/soccer/database/datamodel/Team.java
+++ back-end/src/main/java/com/ser515/soccer/database/datamodel/Team.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-@Entity @Table(uniqueConstraints = { @UniqueConstraint(columnNames = "name") })
+@Entity @Table
 @Getter @Setter @NoArgsConstructor
 public class Team {
     public enum Type { U16, U17, U18, U19, U20, U21, U22, U23 }
@@ -14,7 +14,13 @@ public class Team {
     private long id;
 
     private String name;
+
+    @Enumerated(EnumType.STRING)
     private Type type;
+
+    @ManyToOne @JoinColumn(name = "tournament_id")
+    private Tournament tournament;
+
     /* TODO: Add more details about the team from a team registration request.
         Example: Name of coach, players, etc. */
 }
--- back-end/src/main/java/com/ser515/soccer/database/repository/TeamRepository.java
+++ back-end/src/main/java/com/ser515/soccer/database/repository/TeamRepository.java
@@ -9,5 +9,4 @@ import java.util.Optional;
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
     Optional<Team> findByName(String name);
-    Boolean existsByName(String name);
 }
