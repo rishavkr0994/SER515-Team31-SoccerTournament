6c7c5d4b14bc857f9065e3b08c0101c0da6582a4
rahulvuppla <79802988+rahulvuppla@users.noreply.github.com>
2021-11-27 17:28:01 -0700

    react

---
+++ front-end/soccer/src/components/Home - Copy.js
@@ -0,0 +1,43 @@
+import React from "react";
+import TournamentCard from "./TournamentCard";
+import "./Home.css";
+import { Container, Grid } from "@mui/material";
+export default function Home() {
+  return (
+    <div className="Main">
+      <h1 className="Coming"> Upcoming Tournaments</h1>
+      <Container maxWidth="xl">
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <TournamentCard
+              src="tcard2.jpeg"
+              name="Century Hitters Tournament"
+              description="This is a century tournament!!!"
+              timeRange="January 14th - 16th, 2022"
+              endRegistration="December 21, 2021"
+            ></TournamentCard>
+          </Grid>
+          <Grid item xs={4}>
+            <TournamentCard
+              src="tcard1.png"
+              name="Supersixers League"
+              description="Come and join this excited tournament!"
+              timeRange="January 14th - 16th, 2022"
+              endRegistration="December 21, 2021"
+              isOnGoing="true"
+            ></TournamentCard>
+          </Grid>
+          <Grid item xs={4}>
+            <TournamentCard
+              src="tcard3.jpeg"
+              name="Delight Wings Gaming"
+              description="Come and join this excited tournament!"
+              timeRange="January 14th - 16th, 2022"
+              endRegistration="December 21, 2021"
+            ></TournamentCard>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
