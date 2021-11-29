a923b3198a75c5e45fadd22ba5a5a238142d9374
Runlin Xiao <rxiao13@asu.edu>
2021-11-28 21:25:57 -0700

    adding home page covid guidelines

--- front-end/soccer/src/components/About.js
+++ front-end/soccer/src/components/About.js
@@ -1,9 +1,66 @@
 import React from "react";
-import './FooterAndMain.css'
-export default function About(){
-        return(
-            <div className="Main">
-                About
-            </div>
-        )
+import "./FooterAndMain.css";
+import Grid from "@mui/material/Grid";
+
+export default function About() {
+  return (
+    <div>
+      <div className="Main">
+        <div>
+          <Grid container spacing={2}>
+            <Grid item xs={2}></Grid>
+            <Grid item xs={2}>
+              <img
+                src="firework.gif"
+                style={{ objectFit: "contain", width: "100%" }}
+              ></img>
+            </Grid>
+            <Grid
+              item
+              xs={6}
+              justifyContent="flex-start"
+              display="flex"
+              alignItems="center"
+              sx={{ fontSize: "80px" }}
+            >
+              No.1 league in arizona!
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+      <div style={{height:"400px",backgroundColor:"grey",marginTop:"-70vh",marginBottom:"20vh"}}>
+            <Grid container spacing={2} style={{color:"white"}}>
+              <Grid item  xs={6} className="aboutTitle">
+                  <div>Our mission</div>
+              </Grid>
+              <Grid item  xs={6} className="aboutTitle">
+                  <div>Our essence</div>
+              </Grid>
+              <Grid item  xs={6} className="aboutContent">
+                  <div>Our mission is to provide a positive, competitive youth soccer experience for player, parent and coach. We emphasize long-term player development of physical, emotional and social skills. We believe a positive team sport experience will promote a youth's self-confidence and self-esteem beyond the soccer field.</div>
+              </Grid>
+              <Grid item  xs={6} className="aboutContent">
+                  <div>We try to provide a healthy perspective on both winning and losing. We believe players, parents and coaches will learn more from a competitive 3–2 loss than a non-competitive 7–0 win. Success is defined by improvement, effort, competitive spirit and team play.</div>
+              </Grid>
+              <Grid item xs={12}></Grid>
+              <Grid item xs={12}></Grid>
+              <Grid item xs={12}></Grid>
+              <Grid item xs={12}></Grid>
+
+              <Grid item  xs={6} className="aboutTitle">
+                  <div>Our promise</div>
+              </Grid>
+              <Grid item  xs={6} className="aboutTitle">
+                  <div>Our vibe</div>
+              </Grid>
+              <Grid item  xs={6} className="aboutContent">
+                  <div> To be a model soccer tournament in the Arizona area, with long term plan for existence and service to the community.</div>
+              </Grid>
+              <Grid item  xs={6} className="aboutContent">
+                  <div>Continue to service our members, with caring and joyful spirit and treat everyone in the soccer community with respect and dignity.</div>
+              </Grid>
+            </Grid>
+      </div>
+    </div>
+  );
 }
--- front-end/soccer/src/components/Home.js
+++ front-end/soccer/src/components/Home.js
@@ -37,23 +37,112 @@ export default function Home() {
     });
   }, []);
   return (
-    <div className="Main">
-      <h1 className="Coming"> Upcoming Tournaments</h1>
-      <Grid container spacing={12}>
-        {rows.arr.map((row) => (
-          <Grid item xs={4}>
-            <TournamentCard key={row.name} tournament={row} />
-          </Grid>
-        ))}
-      </Grid>
-      <div
-        style={{ display: "flex", justifyContent: "center", marginTop: "25px" }}
-      >
-        <Button variant="contained" href="/tournament/create" disabled={userInfo.isLoggedIn === false}>
-          Add Tournament
-        </Button>
+    <div>
+      <div className="Main">
+        <h1 className="Coming"> Upcoming Tournaments</h1>
+        <Grid container spacing={12}>
+          {rows.arr.map((row) => (
+            <Grid item xs={4}>
+              <TournamentCard key={row.name} tournament={row} />
+            </Grid>
+          ))}
+        </Grid>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "25px",
+          }}
+        >
+          <Button
+            variant="contained"
+            href="/tournament/create"
+            disabled={userInfo.isLoggedIn === false}
+          >
+            Add Tournament
+          </Button>
+        </div>
       </div>
-      <div style={{width:"100%",height:"300px"}}>
+      <div
+        style={{
+          height: "300px",
+          backgroundColor: "black",
+          marginTop: "-45vh",
+          color: "white",
+        }}
+      >
+        <Grid container spacing={3}>
+          <Grid item xs={4} display="flex" justifyContent="flex-end">
+            <div style={{ fontSize: "40px", marginLeft: "30px" }}>
+              COVID 19 GUIDELINES
+            </div>
+          </Grid>
+          <Grid item xs={8} display="flex" alignContent="flex-end">
+            <div
+              style={{
+                fontSize: "15px",
+                marginLeft: "30px",
+                alignSelf: "center",
+              }}
+            >
+              In compliance with State and county issued updates, everyone must
+              wear a mask inside ‘walk in’ concession stands and bathrooms.
+            </div>
+          </Grid>
+          <Grid item xs={4} display="flex" justifyContent="center">
+            <div
+              style={{
+                alignSelf:"center",
+                border: "1px solid red",
+                width: "200px",
+                // marginLeft:"-200px"
+              }}
+            ></div>
+          </Grid>
+          <Grid item xs={8} display="flex" alignContent="flex-end">
+            <div
+              style={{
+                fontSize: "15px",
+                marginLeft: "30px",
+                alignSelf: "center",
+              }}
+            >
+              Masks are not required outdoors at the complexes whether an
+              individual has been vaccinated or not, however, it is worth
+              considering wearing a mask in crowded outdoor settings and for
+              activities with close contact with others who are not fully
+              vaccinated.
+            </div>
+          </Grid>
+          <Grid item xs={4} display="flex" justifyContent="center">
+            <div style={{ fontSize: "40px", marginLeft: "30px" }}>
+              AUGUST 5TH
+            </div>
+          </Grid>
+          <Grid item xs={8} display="flex" alignContent="flex-end">
+            <div
+              style={{
+                fontSize: "15px",
+                marginLeft: "30px",
+                alignSelf: "center",
+              }}
+            >
+              Fully vaccinated people might choose to mask regardless of the
+              level of transmission, particularly if they or someone in their
+              household is immunocompromised or if someone in their household is
+              unvaccinated.
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: "300px",
+          marginBottom: "50px",
+          backgroundColor: "white",
+        }}
+      >
         <BannerCarousel></BannerCarousel>
       </div>
     </div>
